e4935a95f629ad87ba5baae06da0247ad397fbaf
saqibscriza <saqib@scriza.in>
2025-05-29 12:36:32 +0530

    now changed community and module page

--- src/data/modules.js
+++ src/data/modules.js
@@ -1,201 +1,216 @@
-export const academicsModules = [{
+export const academicsModules = [
+  {
     title: "Class And Section",
-    desc: " A dashboard showcasing various classes and sections enhances productivity while minimizing power a dashboard showcasing various  ",
+    desc: `A centralized dashboard that displays all classes and their respective 
+    sections to enhance administrative productivity and ensure efficient organization.`,
     imageSrc: "./Academics/1.svg",
-},
-{
+  },
+  {
     title: "Syllabus",
-    desc: "Access, upload, and download questions from the question bank effortlessly using our module.",
+    desc: "Access, upload, and manage class-wise syllabi effortlessly using our smart and organized module.",
     imageSrc: "./Academics/2.svg",
-},
-{
+  },
+  {
     title: "Lesson Planning",
-    desc: "Our module generates subject-specific online exams and provides students with fast results.",
+    desc: "Create, manage, and organize subject-wise lesson plans with ease to ensure structured and effective classroom delivery.",
     imageSrc: "./Academics/3.svg",
-},
-{
+  },
+  {
     title: "Exam Timetable",
-    desc: "Teachers use a mobile app to record student attendance, and parents can view daily attendance reports.",
+    desc: "Create and manage class-wise exam schedules effortlessly, ensuring timely communication and organized exam planning for all stakeholders.",
     imageSrc: "./Academics/4.svg",
-},
-{
+  },
+  {
     title: "Admission Management",
-    desc: "Efficiently handle student inquiries, streamline admission procedures, and register new students seamlessly.",
+    desc: "Streamline the entire admission process—from inquiry to enrollment—with a digital, paperless, and efficient system.",
     imageSrc: "./Academics/5.svg",
-},
-{
+  },
+  {
     title: "Circular",
-    desc: "We'll keep parents informed with future updates through the Circular display on the mobile app, accompanied by notifications.",
+    desc: "Easily create, manage, and share important notices and circulars with students, parents, and staff in real-time.",
     imageSrc: "./Academics/6.svg",
-},
-{
+  },
+  {
     title: "Certificate",
-    desc: "Our system facilitates the management and generation of TC and CC for seamless certification processes.",
+    desc: "Generate and issue customized certificates for students and staff with ease using ready-to-use templates.",
     imageSrc: "./Academics/7.svg",
-},
-{
+  },
+  {
     title: "Department",
-    desc: "Incorporate student performance in various reports, complete with signatures from teachers and the principal using our module.",
+    desc: "Manage and organize different school departments efficiently to streamline academic and administrative operations.",
     imageSrc: "./Academics/8.svg",
-},
-{
+  },
+  {
     title: "Customizable Dashboard",
-    desc: "The dashboard provides a quick view of student and staff performance, with detailed data for deeper insights.",
+    desc: "Personalize your dashboard layout and widgets for quick access to the most relevant school data and insights.",
     imageSrc: "./Academics/9.svg",
-}];
-export const studentModules = [{
-    title: "Class And Section",
-    desc: "A dashboard showcasing various classes and sections enhances productivity while minimizing power consumption a dashboeard is showcasing",
-    imageSrc: "./Student/1.svg",
-},
-{
-    title: "Syllabus",
-    desc: "Access, upload, and download questions from the question bank effortlessly using our module.",
-    imageSrc: "./Student/2.svg",
-},
-{
-    title: "Lesson Planning",
-    desc: "Our module generates subject-specific online exams and provides students with fast results.",
-    imageSrc: "./Student/3.svg",
-},
-{
-    title: "Exam Timetable",
-    desc: "Teachers use a mobile app to record student attendance, and parents can view daily attendance reports.",
-    imageSrc: "./Student/4.svg",
-},
-{
-    title: "Admission Management",
-    desc: "Efficiently handle student inquiries, streamline admission procedures, and register new students seamlessly.",
-    imageSrc: "./Student/5.svg",
-},
-{
-    title: "Circular",
-    desc: "We'll keep parents informed with future updates through the Circular display on the mobile app, accompanied by notifications.",
-    imageSrc: "./Student/6.svg",
-},
-{
-    title: "Certificate",
-    desc: "Our system facilitates the management and generation of TC and CC for seamless certification processes.",
-    imageSrc: "./Student/7.svg",
-},
-{
-    title: "Department",
-    desc: "Incorporate student performance in various reports, complete with signatures from teachers and the principal using our module.",
-    imageSrc: "./Student/8.svg",
-},
-{
-    title: "Customizable Dashboard",
-    desc: "The dashboard provides a quick view of student and staff performance, with detailed data for deeper insights.",
-    imageSrc: "./Student/9.svg",
-},];
-export const transportationModules = [{
-    title: "Class And Section",
-    desc: "A dashboard showcasing various classes and sections enhances productivity while minimizing power consumption.",
-    imageSrc: "./Transportation/1.svg",
-},
-{
-    title: "Syllabus",
-    desc: "Access, upload, and download questions from the question bank effortlessly using our module.",
-    imageSrc: "./Transportation/2.svg",
-},
-{
-    title: "Lesson Planning",
-    desc: "Our module generates subject-specific online exams and provides Transportations with fast results.",
-    imageSrc: "./Transportation/3.svg",
-},
-{
-    title: "Exam Timetable",
-    desc: "Teachers use a mobile app to record  attendance, and parents can view daily attendance reports.",
-    imageSrc: "./Transportation/4.svg",
-},
-{
-    title: "Admission Management",
-    desc: "Efficiently handle student inquiries, streamline admission procedures, and register new students seamlessly.",
-    imageSrc: "./Transportation/5.svg",
-},
-{
-    title: "Circular",
-    desc: "We'll keep parents informed with future updates through the Circular display on the mobile app, accompanied by notifications.",
-    imageSrc: "./Transportation/6.svg",
-},
+  },
 ];
-export const examModules = [{
-    title: "Class And Section",
-    desc: "A dashboard showcasing various classes and sections enhances productivity while minimizing power consumption.",
+export const studentModules = [
+  {
+    title: "Assignment and Notes",
+    desc: "Easily create, organize, and distribute assignments and notes for different classes and sections to boost productivity and support student learning.",
+    imageSrc: "./Student/1.svg",
+  },
+  {
+    title: "Event Management",
+    desc: "Plan, schedule, and manage school events seamlessly to ensure smooth coordination and participation across the community.",
+    imageSrc: "./Student/2.svg",
+  },
+  {
+    title: "Task Management",
+    desc: "Organize, assign, and track tasks efficiently to boost productivity and ensure timely completion across departments",
+    imageSrc: "./Student/3.svg",
+  },
+  {
+    title: "Time Table",
+    desc: "Easily create and manage class schedules to optimize learning time and resource allocation.",
+    imageSrc: "./Student/4.svg",
+  },
+  {
+    title: "Store Management",
+    desc: "Efficiently manage school inventory, supplies, and transactions to maintain smooth operations and reduce wastage.",
+    imageSrc: "./Student/5.svg",
+  },
+  {
+    title: "Scholarship",
+    desc: "Manage scholarship applications, approvals, and disbursements to support deserving students effectively.",
+    imageSrc: "./Student/6.svg",
+  },
+  {
+    title: "Student Tracking",
+    desc: "Monitor student attendance, performance, and activities to ensure timely support and intervention.",
+    imageSrc: "./Student/7.svg",
+  },
+  {
+    title: "Email & Notification & Chat",
+    desc: "Stay connected with students, parents, and staff through instant emails, notifications, and real-time chat for effective communication.",
+    imageSrc: "./Student/8.svg",
+  },
+  {
+    title: "Leave Management",
+    desc: "Simplify leave requests, approvals, and tracking for students, teachers, and staff with an easy-to-use system.",
+    imageSrc: "./Student/9.svg",
+  },
+];
+export const transportationModules = [
+  {
+    title: "Vehicle, Driver Details",
+    desc: "Maintain accurate records of vehicles and drivers to ensure safety, compliance, and smooth transportation operations.",
+    imageSrc: "./Transportation/1.svg",
+  },
+  {
+    title: "Vehicle Tracking",
+    desc: "Real-time monitoring of school vehicles to ensure safety, punctuality, and efficient route management.",
+    imageSrc: "./Transportation/2.svg",
+  },
+  {
+    title: "Route Details",
+    desc: "Detailed route planning and tracking for optimized travel paths, ensuring timely pickups and drop-offs with maximum safety.",
+    imageSrc: "./Transportation/3.svg",
+  },
+  {
+    title: "Destination and Fees",
+    desc: "Manage student drop-off points and streamline fee collection for transportation services with ease and transparency.",
+    imageSrc: "./Transportation/4.svg",
+  },
+  {
+    title: "Transportation Allocation",
+    desc: "Assign vehicles and routes to students efficiently to ensure organized and safe daily transportation.",
+    imageSrc: "./Transportation/5.svg",
+  },
+  {
+    title: "Book Category",
+    desc: "Organize library books into categories for easy access, management, and quick retrieval.",
+    imageSrc: "./Transportation/6.svg",
+  },
+];
+export const examModules = [
+  {
+    title: "Manual Examination",
+    desc: "Manage and evaluate paper-based exams efficiently with streamlined processes for scheduling, grading, and result tracking.",
     imageSrc: "./Exam/1.svg",
-},
-{
-    title: "Syllabus",
-    desc: "Access, upload, and download questions from the question bank effortlessly using our module.",
+  },
+  {
+    title: "Question Bank",
+    desc: "Access, create, and organize a repository of questions for easy test and exam preparation.",
     imageSrc: "./Exam/2.svg",
-},
-{
-    title: "Lesson Planning",
-    desc: "Our module generates subject-specific online exams and provides Exams with fast results.",
+  },
+  {
+    title: "Online Examination",
+    desc: "Conduct secure and automated online tests with instant grading and performance analysis.",
     imageSrc: "./Exam/3.svg",
-},
-{
+  },
+  {
     title: "Exam Timetable",
-    desc: "Teachers use a mobile app to record student attendance, and parents can view daily attendance reports.",
+    desc: "Create and manage exam schedules efficiently to ensure smooth and timely assessments.",
     imageSrc: "./Exam/4.svg",
-},
-{
-    title: "Admission Management",
-    desc: "Efficiently handle Exam inquiries, streamline admission procedures, and register new Exams seamlessly.",
+  },
+  {
+    title: "Set Grading/ Ranking Levels",
+    desc: "Define and customize grading and ranking criteria to evaluate student performance accurately and fairly.",
     imageSrc: "./Exam/5.svg",
-},
-{
-    title: "Circular",
-    desc: "We'll keep parents informed with future updates through the Circular display on the mobile app, accompanied by notifications.",
+  },
+  {
+    title: "Exam Result",
+    desc: "Generate and share detailed exam results promptly to keep students and parents informed about academic performance.",
     imageSrc: "./Exam/6.svg",
-},
-{
-    title: "Certificate",
-    desc: "Our system facilitates the management and generation of TC and CC for seamless certification processes.",
+  },
+  {
+    title: "Exam Report",
+    desc: "Provide comprehensive exam reports with insights on student performance to help track progress and identify improvement areas.",
     imageSrc: "./Exam/7.svg",
-},
-{
-    title: "Department",
-    desc: "Incorporate student performance in various reports, complete with signatures from teachers and the principal using our module.",
+  },
+  {
+    title: "Class Designation",
+    desc: "Assign roles and responsibilities to staff and students within each class for organized and efficient management.",
     imageSrc: "./Exam/8.svg",
-},];
-export const hrModules = [{
-    title: "Class And Section",
-    desc: "A dashboard showcasing various classes and sections enhances productivity while minimizing power consumption.",
+  },
+];
+export const hrModules = [
+  {
+    title: "Salary Setting",
+    desc: "Access employee details like designation, pay head, amount type, and unit through our module.",
     imageSrc: "./HR/1.svg",
-},
-{
-    title: "Syllabus",
-    desc: "Access, upload, and download questions from the question bank effortlessly using our module.",
+  },
+  {
+    title: "Pay Head",
+    desc: "Easily display, manage, and organize various master date types using our efficient module system.",
     imageSrc: "./HR/2.svg",
-},
-{
-    title: "Lesson Planning",
-    desc: "Our module generates subject-specific online exams and provides HRs with fast results.",
+  },
+  {
+    title: "Pay Type",
+    desc: "Effortlessly view and manage payment type and name entries using our module. ",
     imageSrc: "./HR/3.svg",
-},
-{
-    title: "Exam Timetable",
-    desc: "Teachers use a mobile app to record HR attendance, and parents can view daily attendance reports.",
+  },
+  {
+    title: "Generate Pay sleep",
+    desc: "Our module generates pay slips and efficiently manages employee reports on a monthly and annual basis.",
     imageSrc: "./HR/4.svg",
-},
-{
-    title: "Admission Management",
-    desc: "Efficiently handle HR inquiries, streamline admission procedures, and register new HRs seamlessly.",
+  },
+  {
+    title: "Account management",
+    desc: "Efficiently track bank and cash transactions, along with expenses and general vouchers using our module.",
     imageSrc: "./HR/5.svg",
-},
-{
-    title: "Circular",
-    desc: "We'll keep parents informed with future updates through the Circular display on the mobile app, accompanied by notifications.",
+  },
+  {
+    title: "Fees Management",
+    desc: "Our module processes school payments, issues fee receipts, and updates the fee register, ensuring highly accurate data.",
     imageSrc: "./HR/6.svg",
-},
-{
-    title: "Certificate",
-    desc: "Our system facilitates the management and generation of TC and CC for seamless certification processes.",
+  },
+  {
+    title: "Reports",
+    desc: "Our module manages reports like Journal, Ledger, Trial Balance, Balance Sheet, and more efficiently.",
     imageSrc: "./HR/7.svg",
-},
-{
-    title: "Department",
-    desc: "Incorporate HR performance in various reports, complete with signatures from teachers and the principal using our module.",
+  },
+  {
+    title: "HR Management",
+    desc: "Our module efficiently manages reports like Journal, Ledger, Trial Balance, Balance Sheet, and more.",
     imageSrc: "./HR/8.svg",
-}];
+  },
+  {
+    title: "Masters",
+    desc: "Our system efficiently manages Account Master, Expense Master, Bank/Cash Master, Tax Master, and other essential components.",
+    imageSrc: "./HR/8.svg",
+  },
+];
